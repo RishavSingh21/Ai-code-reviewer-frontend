1222700869d05eaa39fc317172c8ad7bf13bb4cb
Rishav Kumar <rishavsingh212003@gmail.com>
2026-07-17 00:30:22 +0530

    Update AI Code Reviewer frontend

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
 
-    baseURL: import.meta.env.VITE_API_URL || "https://ai-code-reviewer-backend-ysli.onrender.com//api"
+    baseURL: import.meta.env.VITE_API_URL || "https://ai-code-reviewer-backend-ysli.onrender.com/api"
 
 });
 
